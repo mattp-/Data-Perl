7bb6b7473f977a3c9ebb4e5765eeac052091b817
Matt Phillips <mphillips@oanda.com>
2013-01-11 16:01:21 -0500

    fixups for string and tests

--- lib/Data/Perl/String.pm
+++ lib/Data/Perl/String.pm
@@ -8,33 +8,36 @@ BEGIN {
 
 use strictures 1;
 
-sub new { my $cl = shift; bless(\$_[0]), $cl }
+sub new { my $cl = shift; bless(\$_[0], $cl) }
 
 sub string { Data::Perl::String->new(shift||'') }
 
-sub inc { $$_[0] = $$_[0]++ }
+sub inc { ${$_[0]}++ }
 
-sub append { $$_[0] = $$_[0] . $_[1] }
+sub append { ${$_[0]} = ${$_[0]} . $_[1] }
 
-sub prepend { $$_[0] = $_[1] . $$_[0] }
+sub prepend { ${$_[0]} = $_[1] . ${$_[0]} }
 
 sub replace {
-  ref $_[1]
-    ? $$_[0] =~ s/$$_[0]/$_[1]->()/e
-    : $$_[0] =~ s/$$_[0]/$_[1]/;
+  if (ref($_[2]) eq 'CODE') {
+    ${$_[0]} =~ s/$_[1]/$_[2]->()/e;
+  }
+  else {
+    ${$_[0]} =~ s/$_[1]/$_[2]/;
+  }
 }
 
 sub match {
-    $$_[0] =~ /$_[1]/;
+  ${$_[0]} =~ /$_[1]/;
 }
 
-sub chop { chop $$_[0] }
+sub chop { CORE::chop ${$_[0]} }
 
-sub chomp { chop $$_[0] }
+sub chomp { CORE::chomp ${$_[0]} }
 
-sub clear { $$_[0] = '' }
+sub clear { ${$_[0]} = '' }
 
-sub length { length $$_[0] }
+sub length { CORE::length ${$_[0]} }
 
 sub substr { ... }
 
---
+++ t/data/perl/string.t
@@ -0,0 +1,53 @@
+use Test::More;
+use Data::Perl::String;
+
+# constructor
+is ref(string('a')), 'Data::Perl::String', 'constructor shortcut works';
+
+my $c = string('a');
+is $$c, 'a', 'nondefault set works';
+
+$c->inc;
+is $$c, 'b', 'inc works';
+
+$c = string(123);
+is $$c, '123', 'nondefault set with number works';
+
+# append
+$c->append('z');
+is $$c, '123z', 'append works';
+
+# prepend
+$c->prepend('a');
+is $$c, 'a123z', 'prepend works';
+
+# replace
+$c->replace(qr/2/, 'e');
+is $$c, 'a1e3z', 'replace works';
+$c->replace('3', sub { ok 1, 'sub called in regex replace'; 'r'});
+is $$c, 'a1erz', 'replace works';
+
+# match
+$c = string('5555');
+ok $c->match(qr/5/), 'match works';
+
+# chop
+$c->chop;
+is $$c, '555', 'chop works';
+
+# chomp
+$c = string("foo\n\n\n");
+is $c->chomp, 1, 'chomp works';
+
+# clear
+$c->clear;
+is $$c, '', 'clear works';
+
+
+# length
+$c = string("12345");
+is $c->length, 5, 'length works';
+
+
+
+done_testing();
