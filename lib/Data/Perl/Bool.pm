a29b7ddeb832bdfcb9e4a85b475133821313fb00
Matt Phillips <mphillips@oanda.com>
2013-01-11 09:47:30 -0500

    fixup initial pass of Bool and tests

--- lib/Data/Perl/Bool.pm
+++ lib/Data/Perl/Bool.pm
@@ -8,16 +8,16 @@ BEGIN {
 
 use strictures 1;
 
-sub new { my $cl = shift; bless(\(!!$_[0])), $cl }
+sub new { my $bool = $_[1] ? 1 : 0; bless(\$bool, $_[0]) }
 
 sub bool { Data::Perl::Bool->new(shift||0) }
 
-sub set { $$_[0] = !!$_[1] }
+sub set { ${$_[0]} = 1 }
 
-sub unset { $$_[0] = 0 }
+sub unset { ${$_[0]} = 0 }
 
-sub toggle { $$_[0] = $$_[0] ? 0 : 1; }
+sub toggle { ${$_[0]} = ${$_[0]} ? 0 : 1; }
 
-sub not { $$_[0] = !$$_[0] }
+sub not { !${$_[0]} }
 
 1;
